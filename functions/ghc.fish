56825d7fee216523a0d50ad6b873aad0ca40adf9
Richard Macklin <code@rmacklin.dev>
2024-07-05 22:46:10 -0700

    Update fish function to clean up after a git clone error
    This backports the git clone error-handling of gh.bash from
    75ed770b6128d8465e1f554ba267b33f5fca21fa and
    bbcca827dd45bedb28d7e72abd77a51fab461ba3 to the fish shell.

--- functions/ghc.fish
+++ functions/ghc.fish
@@ -8,18 +8,42 @@ function ghc -d "manage git repos"
   set git_host github.com
 
   if [ (count $argv) -eq 1 ]
-    set repo $argv[1]
+    set -l parts (string split / $argv[1])
+    set user $parts[1]
+    set repo $parts[2]
   else if [ (count $argv) -eq 2 ]
-    set repo $argv[1]/$argv[2]
+    set user $argv[1]
+    set repo $argv[2]
   else
     echo "USAGE: ghc [user] [repo]" >&2
     return 1
   end
 
-  set -l path $gh_base_dir/$git_host/$repo
-  if not test -d $path
-    git clone --recursive git@$git_host:$repo.git $path
+  set -l user_path $gh_base_dir/$git_host/$user
+  if test -d $user_path
+    set user_path_already_existed true
+  else
+    set user_path_already_existed false
   end
 
-  cd $path
+  set -l path $user_path/$repo
+  if not test -d $path
+    git clone --recursive git@$git_host:$user/$repo.git $path
+  end
+
+  # If git exited uncleanly, clean up the created user directory (if it exists)
+  # and don't try to `cd` into it.
+
+  set -l git_exit_code $status
+  if test $git_exit_code -ne 0
+    if test -d $user_path
+      rm -d $user_path/$repo
+      if not $user_path_already_existed
+        rm -d $user_path
+      end
+    end
+    return $git_exit_code
+  else
+    cd $path
+  end
 end
